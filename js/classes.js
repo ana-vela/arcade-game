06f4e283af79fc22aada7de7686b20fb8fbd94dc
anavela <ana@anavela.net>
2018-08-08 20:14:21 -0600

    adding player and enemy

---
+++ js/classes.js
@@ -0,0 +1,63 @@
+class Entity {
+  constructor() {
+    this.sprite = 'images/';
+    this.x = 2;
+    this.y = 5;
+  }
+
+  update(dt) {
+    this.isOutOfBoundsX = this.x > 5;
+    this.isOutOfBoundsY = this.y < 1;
+  }
+
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 83);
+  }
+}
+
+class Player extends Entity {
+  constructor() {
+    super();
+    this.sprite += 'char-boy.png';
+  }
+
+handleInput(input) {
+  switch(input) {
+    case 'left':
+      this.x = this.x > 0 ? this.x -1 : this.x;
+      break;
+    case 'up':
+      this.y = this.y > 0 ? this.y -1 : this.y;
+      break;
+    case 'right':
+      this.x = this.x < 4 ? this.x +1 : this.x;
+      break;
+    case 'down':
+      this.y = this.y < 5 ? this.y +1 : this.y;
+      break;
+    default:
+      break;
+  }
+}
+
+
+}
+
+class Enemy extends Entity {
+    constructor (x, y) {
+      super();
+      this.sprite += 'enemy-bug.png';
+      this.x = x;
+      this.y = y;
+    }
+
+    update(dt) {
+      super.update();
+      if(this.isOutOfBoundsX){
+        this.x = -1;
+      }
+      else {
+        this.x += dt;
+      }
+    }
+  }
